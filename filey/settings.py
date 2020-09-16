2124535416b2bfad52fd6fbb10e6233510cef94b
damephena <okoyeifenna24@gmail.com>
2020-09-17 00:51:58 +0100

    csrf fix

--- filey/settings.py
+++ filey/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'foo')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'fileyy.herokuapp.com']
+CSRF_COOKIE_SECURE = False
 
 AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
